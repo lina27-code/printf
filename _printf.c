4a79eea779622631ee512185eda51b0ee7134f0e
Otu faith kokuwa <f79906212@gmail.com>
2026-04-12 00:09:47 +0100

    Task 9: handle field width

--- _printf.c
+++ _printf.c
@@ -10,163 +10,274 @@ int _printf(const char *format, ...)
 	int i = 0, count = 0;
 	va_list args;
 
-	if (format == NULL)
+	if (!format)
 		return (-1);
 
 	va_start(args, format);
-
 
 	while (format[i])
 	{
 		if (format[i] == '%')
 		{
-        int plus = 0, space = 0, hash = 0;
-        int l = 0, h = 0;
+			int plus = 0, space = 0, hash = 0;
+			int l = 0, h = 0, width = 0;
 
 			i++;
-             
-            /* Handle flags */
-	while (format[i] == '+' || format[i] == ' ' || format[i] == '#')
-	{
-		if (format[i] == '+')
-			plus = 1;
-		else if (format[i] == ' ')
-			space = 1;
-		else if (format[i] == '#')
-			hash = 1;
 
-		i++;
-	} 
-         
-          if (format[i] == 'l')
-{
-	l = 1;
-	i++;
-}
-else if (format[i] == 'h')
-{
-	h = 1;
-	i++;
-}
-	 if (format[i] == 'c')
-	count += _putchar(va_arg(args, int));
+			/* FLAGS */
+			while (format[i] == '+' || format[i] == ' ' || format[i] == '#')
+			{
+				if (format[i] == '+')
+					plus = 1;
+				else if (format[i] == ' ')
+					space = 1;
+				else if (format[i] == '#')
+					hash = 1;
+				i++;
+			}
+
+			/* LENGTH */
+			if (format[i] == 'l')
+			{
+				l = 1;
+				i++;
+			}
+			else if (format[i] == 'h')
+			{
+				h = 1;
+				i++;
+			}
+
+			/* WIDTH */
+			while (format[i] >= '0' && format[i] <= '9')
+			{
+				width = width * 10 + (format[i] - '0');
+				i++;
+			}
+
+			/* ===== SPECIFIERS ===== */
+
+			/* d / i */
+			if (format[i] == 'd' || format[i] == 'i')
+			{
+				long int num, temp;
+				int len = 0;
+
+				if (l)
+					num = va_arg(args, long int);
+				else if (h)
+					num = (short int)va_arg(args, int);
+				else
+					num = va_arg(args, int);
+
+				temp = num;
+
+				if (temp < 0)
+				{
+					len++;
+					temp = -temp;
+				}
+
+				if (temp == 0)
+					len = 1;
+
+				while (temp)
+				{
+					len++;
+					temp /= 10;
+				}
+
+				if (num >= 0 && (plus || space))
+					len++;
+
+				while (width > len)
+				{
+					count += _putchar(' ');
+					width--;
+				}
+
+				if (num >= 0)
+				{
+					if (plus)
+						count += _putchar('+');
+					else if (space)
+						count += _putchar(' ');
+				}
+
+				count += print_number(num);
+			}
+
+			/* u */
+			else if (format[i] == 'u')
+			{
+				unsigned long int num, temp;
+				int len = 0;
+
+				if (l)
+					num = va_arg(args, unsigned long int);
+				else if (h)
+					num = (unsigned short int)va_arg(args, unsigned int);
+				else
+					num = va_arg(args, unsigned int);
+
+				temp = num;
+
+				if (temp == 0)
+					len = 1;
+
+				while (temp)
+				{
+					len++;
+					temp /= 10;
+				}
+
+				while (width > len)
+				{
+					count += _putchar(' ');
+					width--;
+				}
+
+				count += print_unsigned(num);
+			}
+
+			/* x */
+			else if (format[i] == 'x')
+			{
+				unsigned long int num, temp;
+				int len = 0;
+
+				if (l)
+					num = va_arg(args, unsigned long int);
+				else if (h)
+					num = (unsigned short int)va_arg(args, unsigned int);
+				else
+					num = va_arg(args, unsigned int);
+
+				temp = num;
+
+				if (temp == 0)
+					len = 1;
+
+				while (temp)
+				{
+					len++;
+					temp /= 16;
+				}
+
+				if (hash && num != 0)
+					len += 2;
+
+				while (width > len)
+				{
+					count += _putchar(' ');
+					width--;
+				}
+
+				if (hash && num != 0)
+				{
+					count += _putchar('0');
+					count += _putchar('x');
+				}
+
+				count += print_base(num, 16, 0);
+			}
+
+			/* X */
+			else if (format[i] == 'X')
+			{
+				unsigned long int num, temp;
+				int len = 0;
+
+				if (l)
+					num = va_arg(args, unsigned long int);
+				else if (h)
+					num = (unsigned short int)va_arg(args, unsigned int);
+				else
+					num = va_arg(args, unsigned int);
+
+				temp = num;
+
+				if (temp == 0)
+					len = 1;
+
+				while (temp)
+				{
+					len++;
+					temp /= 16;
+				}
+
+				if (hash && num != 0)
+					len += 2;
+
+				while (width > len)
+				{
+					count += _putchar(' ');
+					width--;
+				}
+
+				if (hash && num != 0)
+				{
+					count += _putchar('0');
+					count += _putchar('X');
+				}
+
+				count += print_base(num, 16, 1);
+			}
+
+			/* o */
+			else if (format[i] == 'o')
+			{
+				unsigned long int num, temp;
+				int len = 0;
+
+				if (l)
+					num = va_arg(args, unsigned long int);
+				else if (h)
+					num = (unsigned short int)va_arg(args, unsigned int);
+				else
+					num = va_arg(args, unsigned int);
+
+				temp = num;
+
+				if (temp == 0)
+					len = 1;
+
+				while (temp)
+				{
+					len++;
+					temp /= 8;
+				}
+
+				if (hash && num != 0)
+					len++;
+
+				while (width > len)
+				{
+					count += _putchar(' ');
+					width--;
+				}
+
+				if (hash && num != 0)
+					count += _putchar('0');
+
+				count += print_base(num, 8, 0);
+			}
+
+			/* others */
+			else if (format[i] == 'c')
+				count += _putchar(va_arg(args, int));
 
 			else if (format[i] == 's')
 			{
 				char *str = va_arg(args, char *);
 				int j = 0;
 
-	if (str == NULL)
+				if (!str)
 					str = "(null)";
 
-	while (str[j])
-				{
-					count += _putchar(str[j]);
-					j++;
-				}
+				while (str[j])
+					count += _putchar(str[j++]);
 			}
-
-	else if (format[i] == 'd' || format[i] == 'i')
-{
-	long int num;
-
-	if (l)
-		num = va_arg(args, long int);
-	else if (h)
-		num = (short int)va_arg(args, int);
-	else
-		num = va_arg(args, int);
-
-	if (num >= 0)
-	{
-		if (plus)
-			count += _putchar('+');
-		else if (space)
-			count += _putchar(' ');
-	}
-
-	count += print_number(num);
-}	
-
-			else if (format[i] == 'b')
-				count += print_binary(va_arg(args, unsigned int));
-
-    else if (format[i] == 'u')
-	{
-	unsigned long int num;
-
-	if (l)
-		num = va_arg(args, unsigned long int);
-	else if (h)
-		num = (unsigned short int)va_arg(args, unsigned int);
-	else
-		num = va_arg(args, unsigned int);
-
-	count += print_unsigned(num);
-}
-
-else if (format[i] == 'o')
-	{
-
-	unsigned long int num;
-
-	if (l)
-		num = va_arg(args, unsigned long int);
-	else if (h)
-		num = (unsigned short int)va_arg(args, unsigned int);
-	else
-		num = va_arg(args, unsigned int);
-
-	if (hash && num != 0)
-		count += _putchar('0');
-
-	count += print_base(num, 8, 0);
-}
-
-else if (format[i] == 'x')
-	{
-
-	unsigned long int num;
-
-	if (l)
-		num = va_arg(args, unsigned long int);
-	else if (h)
-		num = (unsigned short int)va_arg(args, unsigned int);
-	else
-		num = va_arg(args, unsigned int);
-
-	if (hash && num != 0)
-				{
-		count += _putchar('0');
-		count += _putchar('x');
-				}
-
-		count += print_base(num, 16, 0);
-}
-
-else if (format[i] == 'X')
-{
-	unsigned long int num;
-
-	if (l)
-		num = va_arg(args, unsigned long int);
-	else if (h)
-		num = (unsigned short int)va_arg(args, unsigned int);
-	else
-		num = va_arg(args, unsigned int);
-
-	if (hash && num != 0)
-{
-	    count += _putchar('0');
-		count += _putchar('X');
-}
-    count += print_base(num, 16, 1);
-}	
-
-else if (format[i] == 'S')
-	count += print_string_special(va_arg(args, char *));
-else if (format[i] == 'p')
-	count += print_pointer(va_arg(args, void *));
 
 			else if (format[i] == '%')
 				count += _putchar('%');
@@ -178,15 +289,13 @@ else if (format[i] == 'p')
 			}
 		}
 		else
-		{
 			count += _putchar(format[i]);
-		}
 
 		i++;
 	}
 
 	va_end(args);
-    _putchar(-1);
+	_putchar(-1);
 
 	return (count);
 }
